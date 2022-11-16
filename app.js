1ed69721e78eed62ebe6e5505ca46982028a5d91
François Florent <francois.florent.01@gmail.com>
2022-11-16 17:23:07 -0500

    Main fuctionality working

--- app.js
+++ app.js
@@ -2,18 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8081;
 
-// const cors = require('cors');
-// const routes = require('./routes')
-// app.use(cors());
-// app.use(routes);
+app.use(express.static('static'))
+// app.use(express.static('static/js'))
 
 
-
-app.use(express.static(__dirname))
-console.log(__dirname)
-
-
-app.listen(8000, () => {
+app.listen(port, () => {
     console.log(`Server started listening on port: ${port}`)
   })
-  
--- static/js/selectQuote.mjs
+++ static/js/selectQuote.mjs
@@ -1,6 +1,46 @@
-'use strict'
+let quoteData
 
-// const fs = require('fs');
+fetch("../data/data.json")
+    .then(res => res.json())
+    .then(data => {
+        quoteData = data
+    })
 
-const quote_data = require("../data/data.json")
+const selectedQuote = {}
 
+const selectQuote = (event) => {
+    const category = event.target.id
+    const quotesInCategory = quoteData[category]
+
+    // Quote Selection
+    if ( !(category in selectedQuote) ) {
+        selectedQuote[category] = quotesInCategory[Math.floor(Math.random() * quotesInCategory.length)]
+    }
+    
+    // Set the quote to <blockquote>
+    
+    const [citation, author] = selectedQuote[category]
+    console.log(citation, author)
+    const figure = document.getElementById("today_quote")
+    const blockquote = figure.querySelector("blockquote")
+    const figcaption = figure.querySelector("figcaption")
+
+    blockquote.innerHTML = citation
+    figcaption.innerHTML = "- " + author
+
+    figure.style.display = "block"
+
+    // Toggle background-color of selected li item
+    const liList = event.target.parentNode.getElementsByTagName('li')
+    Object.values(liList).forEach(elem => {
+        if (elem === event.target) {
+            elem.style.backgroundColor = "lightgreen"
+        } else {
+            elem.style.backgroundColor = ""
+        }
+    })
+ 
+}
+
+document.getElementById("animal").addEventListener("click", selectQuote)
+document.getElementById("feel_good").addEventListener("click", selectQuote)
